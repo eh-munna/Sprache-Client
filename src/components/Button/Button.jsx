6ab5e8226f01eb1c06fb20f84cbd14e6a5a74d0d
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-06-10 15:15:56 +0200

    error page done

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,5 +1,9 @@
 const Button = ({ children }) => {
-  return <button className="px-2 py-1 rounded bg-[#118ab2]">{children}</button>;
+  return (
+    <button className="font-[roboto] bg-[#7371fc] rounded-full py-1 px-4 hover:bg-[#3c096c] text-base md:text-lg text-[#fff]">
+      {children}
+    </button>
+  );
 };
 
 export default Button;
--- src/shared/ErrorPage/ErrorPage.jsx
+++ src/shared/ErrorPage/ErrorPage.jsx
@@ -1,24 +1,24 @@
 import { Link } from 'react-router-dom';
+import Button from '../../components/Button/Button';
 
 const ErrorPage = () => {
   return (
     <div className="container mx-auto p-2 md:p-4">
       <div className="flex flex-col items-center justify-between space-y-8">
-        <h1 className="font-[archivo] py-8 md:py-12 text-2xl md:text-4xl font-bold text-[#f77f00]">
-          Opps!!!
-        </h1>
-
-        <img
-          className="max-w-full rounded-lg shadow-2xl"
-          src="https://i.ibb.co/TLc9Xmy/error-404.jpg"
-          alt=""
-        />
-        <p className="text-lg text-gray-500">Page not found</p>
+        <div className="pt-6 md:pt-12">
+          <img
+            className="max-w-full rounded-lg shadow-2xl"
+            src="https://i.ibb.co/y8h5cgc/46422844.jpg"
+            alt=""
+          />
+        </div>
+        <p className="text-lg font-[roboto] text-[#4361ee]">
+          The page you are looking for was moved, removed, renamed or might
+          never existed
+        </p>
 
         <Link to="/">
-          <button className="font-[roboto] bg-[#00b4d8]  rounded-md p-2 text-base md:text-lg text-[#fff]">
-            Let&apos;s explore Home
-          </button>
+          <Button>Let&apos;s go home</Button>
         </Link>
       </div>
     </div>
