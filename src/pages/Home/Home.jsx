8e16c4d04d07363bdfc82a3b5c274bfd78f2e0d5
ehmunna <emran-hussain.munna-tu-ilmenau.de>
2023-06-12 15:52:27 +0200

    home slider is done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,5 +1,33 @@
+import { useState } from 'react';
+import useLoadUsers from '../../hooks/useLoadUsers';
+
 const Home = () => {
-  return <div>fjldsjf</div>;
+  const [isBtnDisabled, setBtnDisabled] = useState(false);
+  // const [users] = useLoadUsers();
+  // console.log(users);
+
+  // // if (user.adminRole === true) {
+  // //   setBtnDisabled(true);
+  // // }
+
+  // const tryingg = users.map((user) => {
+  //   if (user.adminRole) {
+  //     setBtnDisabled(true);
+  //     return user.adminRole;
+  //   }
+  // });
+
+  const test = () => {
+    console.log(`button is not disabled`);
+  };
+
+  return (
+    <div>
+      <button onClick={test} className="border p-3" disabled={isBtnDisabled}>
+        Test Button
+      </button>
+    </div>
+  );
 };
 
 export default Home;
--- src/pages/Home/HomeSlider/HomeSlider.jsx
+++ src/pages/Home/HomeSlider/HomeSlider.jsx
@@ -3,13 +3,28 @@ import { useKeenSlider } from 'keen-slider/react';
 import 'keen-slider/keen-slider.min.css';
 import './HomeSlider.css';
 import { AuthContext } from '../../../providers/AuthProvider';
+import Button from '../../../components/Button/Button';
 
 const images = [
-  'https://images.unsplash.com/photo-1590004953392-5aba2e72269a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80',
-  'https://images.unsplash.com/photo-1590004845575-cc18b13d1d0a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80',
-  'https://images.unsplash.com/photo-1590004987778-bece5c9adab6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80',
-  'https://images.unsplash.com/photo-1590005176489-db2e714711fc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&h=500&w=800&q=80',
+  'https://i.ibb.co/mRR6rQ5/book-1.jpg',
+  'https://i.ibb.co/h9tn9Dm/book-2.jpg',
+  'https://i.ibb.co/gzvdLnr/book-3.jpg',
+  'https://i.ibb.co/zGPTvbL/book-4.jpg',
+  'https://i.ibb.co/T2xDsNj/book-5.webp',
 ];
+
+const textContent = [
+  'Learn a language that connects cultures',
+  'Explore a rich language of history and tradition',
+  'Our methods make German learning enjoyable',
+  'Join our community for an authentic experience',
+  'Gain a competitive edge in a globalized world',
+];
+
+// const hello = textContent.map((text) => {
+//   <h2 className="top-1/2 absolute right-1/2">{text}</h2>;
+//   return text;
+// });
 
 const HomeSlider = () => {
   const [opacities, setOpacities] = useState([]);
@@ -24,19 +39,44 @@ const HomeSlider = () => {
       setOpacities(new_opacities);
     },
   });
+  const [textRef] = useKeenSlider({
+    slides: textContent.length,
+    loop: true,
+    detailsChanged(s) {
+      const new_opacities = s.track.details.slides.map(
+        (slide) => slide.portion
+      );
+      setOpacities(new_opacities);
+    },
+  });
 
   return (
-    <div ref={sliderRef} className={`fader `}>
-      {images.map((src, idx) => (
-        <div
-          key={idx}
-          className="fader__slide"
-          style={{ opacity: opacities[idx] }}
-        >
-          <img src={src} />
-        </div>
-      ))}
-    </div>
+    <>
+      <div ref={sliderRef} className={`fader `}>
+        {images.map((src, idx) => (
+          <div
+            key={idx}
+            className="fader__slide bg-black text-white"
+            style={{ opacity: opacities[idx] }}
+          >
+            <div>
+              <img className="opacity-30" src={src} />
+            </div>
+            <div ref={textRef}>
+              {textContent.map((text, idx) => (
+                <h2
+                  key={idx}
+                  className="fader__slide flex justify-center items-center text-lg md:text-4xl font-[archivo] text-gray-200 px-2"
+                  style={{ opacity: opacities[idx] }}
+                >
+                  {text}
+                </h2>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
